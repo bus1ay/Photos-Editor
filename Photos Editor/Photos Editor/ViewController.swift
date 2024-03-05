55a1ef00b6a6203167dc50a5826446b70c948d00
bus1ay <buslaevigor2003@gmail.com>
2024-03-05 18:39:10 +0300

    3) Добавили в сториборд UIImage и кнопку для редактирования + кнопку для добавления фото

--- Photos Editor/Photos Editor/ViewController.swift
+++ Photos Editor/Photos Editor/ViewController.swift
@@ -7,12 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var picture: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func addAction(_ sender: Any) {
+    }
+    
+    @IBAction func editAction(_ sender: Any) {
+    }
+    
+}
 
+extension ViewController: ImagePickerDelegate {
+    func didSelect(image: UIImage?) {
+        <#code#>
+    }
+    
+    
 }
 
