00d13273b17d48ab84c7b1a269059c177fdb7edc
bus1ay <buslaevigor2003@gmail.com>
2024-03-05 19:55:10 +0300

    5) Добавили фильтры и кнопку возврата оригинального вида изображения

--- Photos Editor/Photos Editor/Editor/EditorViewController.swift
+++ Photos Editor/Photos Editor/Editor/EditorViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class EditorViewController: UIViewController {
     
+    @IBOutlet weak var filterView: UIView!
+    
     public var picture: UIImage?
     
     
@@ -17,21 +19,31 @@ class EditorViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.ImageView.image = picture
+        filterView.backgroundColor = UIColor(_colorLiteralRed: 0, green: 0, blue: 0, alpha: 0)
     }
     
     @IBAction func action1(_ sender: Any) {
+        filterView.backgroundColor = UIColor.Filter.blue
     }
     
     @IBAction func action2(_ sender: Any) {
+        filterView.backgroundColor = UIColor.Filter.gray
     }
     
     @IBAction func action3(_ sender: Any) {
+        filterView.backgroundColor = UIColor.Filter.orange
     }
     
     @IBAction func action4(_ sender: Any) {
+        filterView.backgroundColor = UIColor.Filter.purple
     }
     
     @IBAction func action5(_ sender: Any) {
+        filterView.backgroundColor = UIColor.Filter.yellow
+    }
+    
+    @IBAction func clearButton(_ sender: Any) {
+        filterView.backgroundColor = UIColor(_colorLiteralRed: 0, green: 0, blue: 0, alpha: 0)
     }
     
     
